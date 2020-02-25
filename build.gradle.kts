c9339327e72677a34f42a90e36e1017a54841d18
yt8492 <tmymyut2000@gmail.com>
2020-02-25 20:17:20 +0900

    add runTask

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,9 @@ kotlin {
             webpackTask {
                 outputFileName = "main.js"
             }
+            runTask {
+                outputFileName = "main.js"
+            }
         }
     }
 }
